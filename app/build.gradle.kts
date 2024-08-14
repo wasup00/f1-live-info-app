e4bb365f7573d6d24a4ccf0ac5204d63095d5948
Guillaume DHOTEL <guillaume.dhotel1@gmail.com>
2024-08-14 10:32:09 +0200

    Changed SDK target to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.f1liveinfo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.f1liveinfo"
         minSdk = 34
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
